1be3ba8742b5d2104049d5fcc099b86558b8f704
Jordan Wilberding <diginux@gmail.com>
2009-10-31 23:26:58 -0500

    gen server added and a config file

---
+++ src/config.hrl
@@ -0,0 +1,12 @@
+-ifndef(beeerme_config_hrl).
+-define(beeerme_config_hrl, ok).
+
+
+-define(HOST, "localhost").
+%%-define(HOST, "beeer.me").
+-define(SERVER_PORT, 9630).
+
+%% Protocol
+-define(REQ_HASH_LIST, 5).
+
+-endif.
